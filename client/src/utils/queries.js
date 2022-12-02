6c381818b4e452638de14bba79469e58aa5172cd
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-12-02 17:40:38 -0600

    created friendlist and reactionlist component and added routes

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_THOUGHTS = gql`
-  query thoughts($username: String) {
-    thoughts(username: $username) {
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
       _id
       thoughtText
       createdAt
@@ -13,6 +13,27 @@ export const QUERY_THOUGHTS = gql`
         createdAt
         username
         reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
       }
     }
   }
